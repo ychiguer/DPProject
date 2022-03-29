3bb3cfa08900b33efdac0c6e22f65d25e1359622
Youssef Chiguer <80593002+ychiguer@users.noreply.github.com>
2022-03-29 21:49:39 +0200

    Add files via upload

---
+++ init.py
@@ -0,0 +1,14 @@
+import PIL
+import os
+
+classes = ["TAT", "MOL", "PIG"];
+
+for c in classes:
+    file = 1
+    for pic in os.listdir(".//"+c):
+        img = PIL.Image.open(".//"+c + "//" + pic)
+        img = img.resize((600,600))
+        img.save(".//"+c + "//" + str(file) +".png")
+        if (pic != str(file) +".png" ):
+            os.remove(".//"+c + "//" + pic)
+        file += 1
